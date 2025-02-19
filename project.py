9c16bba9d9538871f6cf83ef4dbeb657f229666c
vbystrov <vladimir.bystrov20180@yandex.ru>
2025-02-19 18:57:37 +0300

    Добавил переход по папкам.

--- project.py
+++ project.py
@@ -1,37 +1,81 @@
-
 from flask import Flask, render_template_string, send_from_directory
 import os
 
 app = Flask(__name__)
 
-folder_path = r'C:\Users\admin\Downloads'
+base_folder_path = '/mnt/logs'
 
+def is_directory(path):
+    return os.path.isdir(path)
+
+# Главная страница
 @app.route('/')
 def index():
     try:
-        files = os.listdir(folder_path)
+        files = os.listdir(base_folder_path)
+        files_info = [{'name': file, 'is_directory': is_directory(os.path.join(base_folder_path, file))} for file in files]
     except FileNotFoundError:
         return "Папка не найдена", 404
 
     html_content = '''
     <html>
-        <head><title>Логи контейнеров</title></head>
+        <head><title>Логирование контейнеров</title></head>
         <body>
-            <h1>Доступные контейнеры: {{ folder }}</h1>
+            <h1>Доступные контейнеры</h1>
             <ul>
                 {% for file in files %}
-                    <li><a href="/files/{{ file }}">{{ file }}</a></li>
+                    <li>
+                        {% if file.is_directory %}
+                            <a href="/folder/{{ file.name }}">{{ file.name }}</a>
+                        {% else %}
+                            {{ file.name }}
+                        {% endif %}
+                    </li>
                 {% endfor %}
             </ul>
         </body>
     </html>
     '''
-    return render_template_string(html_content, folder=folder_path, files=files)
+    return render_template_string(html_content, files=files_info)
 
-@app.route('/files/<filename>')
-def serve_file(filename):
+@app.route('/folder/<path:folder_path>')
+def show_folder(folder_path):
+    full_path = os.path.join(base_folder_path, folder_path)
+
     try:
-        return send_from_directory(folder_path, filename)
+        files = os.listdir(full_path)
+        files_info = [{'name': file, 'is_directory': is_directory(os.path.join(full_path, file))} for file in files]
+    except FileNotFoundError:
+        return "Папка не найдена", 404
+
+    html_content = '''
+    <html>
+        <head><title>Содержимое контейнера: {{ folder }}</title></head>
+        <body>
+            <h1>Содержимое контейнера: {{ folder }}</h1>
+            <ul>
+                <li><a href="/">Назад</a></li>
+                {% for file in files %}
+                    <li>
+                        {% if file.is_directory %}
+                            <a href="/folder/{{ folder }}/{{ file.name }}">{{ file.name }}</a>
+                        {% else %}
+                            <a href="/folder/{{ folder }}/files/{{ file.name }}">{{ file.name }}</a>
+                        {% endif %}
+                    </li>
+                {% endfor %}
+            </ul>
+        </body>
+    </html>
+    '''
+    return render_template_string(html_content, folder=folder_path, files=files_info)
+
+@app.route('/folder/<path:folder_path>/files/<filename>')
+def serve_file(folder_path, filename):
+    try:
+        full_path = os.path.join(base_folder_path, folder_path, filename)
+        return send_from_directory(os.path.join(base_folder_path, folder_path), filename)
+    
     except FileNotFoundError:
         return "Файл не найден", 404
 
